7aca71da1476cbda135db02b5191e23745bf7d0f
Yasa Bharathsai <yasabharathsai@gmail.com>
2026-07-05 10:06:04 +0530

    Updated production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://url-shortener-api-bk8f.onrender.com/docs"
+    "https://url-shortener-api-bk8f.onrender.com"
 }); 
